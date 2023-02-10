e54ef9d25df8f3dd480908366040e29094591232
Guyo3310 <g.abduba43@gmail.com>
2023-02-10 12:34:55 +0300

    in memory database connection

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express()
 const PORT = 3000;
+
+const db = new sqlite3.Database(':memory:');
+
+db.serialize(function() {
+    db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT, balance REAL)");
+    db.run("CREATE TABLE IF NOT EXISTS transactions (id INTEGER PRIMARY KEY, user_id INTEGER, type TEXT, amount REAL, date TEXT)");
+  });
 
 app.listen(PORT, () =>{
     console.log('Bank API Server Running.................')
