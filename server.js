f3764176c191571e3db07d6e3e6c905acafcf549
Guyo3310 <g.abduba43@gmail.com>
2023-02-10 12:38:55 +0300

    in-memory database connection successfull

--- server.js
+++ server.js
@@ -4,7 +4,19 @@ const sqlite3 = require('sqlite3').verbose();
 const app = express()
 const PORT = 3000;
 
-const db = new sqlite3.Database(':memory:');
+const MAX_DEPOSIT_AMOUNT = 40000;
+const MAX_DEPOSIT_FREQUENCY = 4;
+const MAX_DEPOSIT_TOTAL = 150000;
+const MAX_WITHDRAWAL_AMOUNT = 20000;
+const MAX_WITHDRAWAL_FREQUENCY = 3;
+const MAX_WITHDRAWAL_TOTAL = 50000;
+
+const db = new sqlite3.Database(':memory:', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+  });
 
 db.serialize(function() {
     db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT, balance REAL)");
